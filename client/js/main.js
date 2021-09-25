bc39c9ce453c67be749439a74d19b2f7a0e66710
longjichen1 <chenjerry222@gmail.com>
2021-09-24 21:12:41 -0500

    fixed game window and draggables

--- client/js/main.js
+++ client/js/main.js
@@ -1,13 +1,51 @@
-const choices = document.querySelectorAll('.choice')
+// https://github.com/ayyazzafar/sortable-drag-and-drop-todo-list-with-javascript
+let cards = document.querySelectorAll('.choice');
+let lists = document.querySelectorAll('.choice-container');
 
 
-choices.forEach(choice =>{
-    console.log('hi')
-    const p = choice.querySelector('p')
-    choice.addEventListener('dragstart', () =>{
-        p.innerText = "hi"
-    })
-    choice.addEventListener('dragend', () =>{
-        p.innerText="1"
-    })
-})
+cards.forEach((card)=>{
+    registerEventsOnCard(card);
+});
+
+lists.forEach((list)=>{
+    list.addEventListener('dragover', (e)=>{
+        e.preventDefault();
+        let draggingCard = document.querySelector('.dragging');
+        let cardAfterDraggingCard = getCardAfterDraggingCard(list, e.clientY);
+        if(cardAfterDraggingCard){
+            cardAfterDraggingCard.parentNode.insertBefore(draggingCard, cardAfterDraggingCard);
+        } else{
+            list.appendChild(draggingCard);
+        }
+        
+    });
+});
+
+function getCardAfterDraggingCard(list, yDraggingCard){
+
+    let listCards = [...list.querySelectorAll('.choice:not(.dragging)')];
+
+    return listCards.reduce((closestCard, nextCard)=>{
+        let nextCardRect = nextCard.getBoundingClientRect();
+        let offset = yDraggingCard - nextCardRect.top - nextCardRect.height /2;
+
+        if(offset < 0 && offset > closestCard.offset){
+            return {offset, element: nextCard}
+        } else{
+            return closestCard;
+        }
+    
+    }, {offset: Number.NEGATIVE_INFINITY}).element;
+
+}
+
+function registerEventsOnCard(card){
+    card.addEventListener('dragstart', (e)=>{
+        card.classList.add('dragging');
+    });
+
+
+    card.addEventListener('dragend', (e)=>{
+        card.classList.remove('dragging');
+    });
+}
